ebc788879db7571942b109063071430028fa8840
mparry27 <34042760+mparry27@users.noreply.github.com>
2020-07-25 11:11:59 -0600

    Added javascript->java functionality.

--- src/main/java/gui/controllers/MainController.java
+++ src/main/java/gui/controllers/MainController.java
@@ -1,8 +1,10 @@
 package gui.controllers;
 
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.web.WebEngine;
@@ -17,7 +19,7 @@ public class MainController {
     private WebView video;
 
     @FXML
-    private Label label;
+    private Slider timeLine;
 
     @FXML
     private Button play;
@@ -38,15 +40,31 @@ public class MainController {
     @FXML
     private void initialize()
     {
-        engine = video.getEngine();
-        engine.load(getClass().getResource(("/Video.html")).toString());
-
-        page = (JSObject)engine.executeScript("window");
-        page.setMember("app", this);
-
         play.setGraphic(playIcon);
         forw.setGraphic(forwIcon);
         prev.setGraphic(prevIcon);
+
+        engine = video.getEngine();
+        engine.load(getClass().getResource(("/Video.html")).toString());
+
+        engine.getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
+            if (Worker.State.SUCCEEDED == newValue) {
+                page = (JSObject) engine.executeScript("window");
+                page.setMember("javaConnector", this);
+            }
+        });
+
+        timeLine.valueProperty().addListener((obs, oldValue, newValue) -> {
+            engine.executeScript("player.seekTo(" + newValue.doubleValue() + ", true)");
+        });
+    }
+
+    @FXML
+    public void startVideo() {
+        timeLine.setValue(0);
+        play.setGraphic(pauseIcon);
+        timeLine.setMax((double)engine.executeScript("player.getDuration()"));
+
     }
 
     @FXML
